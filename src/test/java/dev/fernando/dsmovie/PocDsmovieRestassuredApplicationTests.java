64fd7317f43c8e76d089b4f5bda008637f7eb3e6
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-10-29 21:57:50 -0300

    first commit

---
+++ src/test/java/dev/fernando/dsmovie/PocDsmovieRestassuredApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fernando.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDsmovieRestassuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
